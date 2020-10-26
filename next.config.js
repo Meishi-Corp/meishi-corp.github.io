88e5025787a7dd31d55d909a06aa3f63bcfd9fd8
Huguenin-Elie Steve <steve.huguenin-elie@protonmail.ch>
2020-10-26 16:56:40 +0100

    add files for gh pages deployment and enabled site to run as PWA next.js instead of gatsby

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const debug = process.env.NODE_ENV !== 'production'
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
-})
+module.exports = {
+  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
+  withPWA: ({
+    pwa: {
+      dest: 'public',
+      runtimeCaching,
+    },
+  })
+}
