461f79e3719f51375e3737ccc5d74b90260d950d
Huguenin-Elie Steve <steve.huguenin-elie@protonmail.ch>
2020-10-26 16:35:38 +0100

    footer and main are separate

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Main from '../components/Main'
+import Footer from '../components/Footer'
+import styles from '../components/main.module.scss'
 
 export default function Home() {
   return (
-    <div className="app">
+    <div className={styles.container} >
       <Main />
+      <Footer />
     </div>
   )
 }
