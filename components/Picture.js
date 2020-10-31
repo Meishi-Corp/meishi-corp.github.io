6c8d4fc95784737faf8d482cc7141fcda5d00239
Huguenin-Elie Steve <steve.huguenin-elie@protonmail.ch>
2020-10-31 22:01:28 +0100

    changed favicon to meishi iching

--- components/Picture.js
+++ components/Picture.js
@@ -4,7 +4,7 @@ export default function Picture () {
   return (
     <>
       <center>
-        <img src="/i_ching_Meishi.png" alt="I-Ching Meishi" className={styles.picture} />
+        <img src="/meishi.png" alt="I-Ching Meishi" className={styles.picture} />
         <p>Meishi (名刺) comes from Japanese and can be translated as Business card.</p>
       </center>
     </>
